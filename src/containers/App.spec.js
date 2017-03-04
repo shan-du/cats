e6378ae530b049ec2ec4b7c139cdf8847c4979f8
Shan Du <shan.du.sdu@gmail.com>
2017-03-04 15:58:41 -0500

    add first unit test

--- src/containers/App.spec.js
+++ src/containers/App.spec.js
@@ -1,10 +1,16 @@
 // Must have at least one test file in this directory or Mocha will throw an error.
-// import React from 'react';
-// import {shallow} from 'enzyme';
-// import App from './App';
+import React from 'react';
+import {shallow} from 'enzyme';
+import App from './App';
 
 describe('<App />', () => {
-  it('should exist', () => {
-    expect(true).toEqual(true);
+  it('should contain its children', () => {
+    const children = (<p>test</p>);
+    const component = shallow(
+      <App>
+        {children}
+      </App>
+    );
+    expect(component.contains(children)).toEqual(true);
   });
 });
